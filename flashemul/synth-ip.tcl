e442897cc94036d37a5fb3153c12affe9fe7c28d
Jamey Hicks <jamey.hicks@qrclab.com>
2014-07-14 10:39:45 -0400

    added synth_ip ddr3

--- flashemul/synth-ip.tcl
+++ flashemul/synth-ip.tcl
@@ -1,5 +1,5 @@
 source board.tcl
 source $xbsvdir/scripts/xbsv-synth-ip.tcl
 
-xbsv_synth_ip aurora_64b66b 8.3 aurora_64b66b_0 [list CONFIG.interface_mode {Framing} CONFIG.C_GT_LOC_5 {1} CONFIG.C_GT_LOC_1 {X}]
-#xbsv_synth_ip ddr3 1.7 ddr3_0 [list CONFIG.interface_mode {Framing} ]
+#xbsv_synth_ip aurora_64b66b 8.3 aurora_64b66b_0 [list CONFIG.interface_mode {Framing} CONFIG.C_GT_LOC_5 {1} CONFIG.C_GT_LOC_1 {X}]
+xbsv_synth_ip mig_7series 2.0 ddr3_0 [list CONFIG.BOARD_MIG_PARAM Custom CONFIG.XML_INPUT_FILE "$boardname-ddr3-mig.prj" CONFIG.Component_Name ddr3_0 CONFIG.RESET_BOARD_INTERFACE Custom ]
